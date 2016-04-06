2d64552f25b7872552ffa9aea10bc171ab91451a
LydiePrevost <lydie.prevost@gmail.com>
2016-04-06 16:11:15 -0700

    removed line

--- src/FibonacciCalculator.java
+++ src/FibonacciCalculator.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
  * and Iteration.
  * Fibonacci number is sum of previous two Fibonacci numbers fn= fn-1+ fn-2
  * first 10 Fibonacci numbers are 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
- *
  */
 public class FibonacciCalculator {
 
